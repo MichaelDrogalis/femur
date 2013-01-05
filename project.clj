1eb2334be47434b0d512a12c4c835441d1b62681
Michael Drogalis <mjd3089@rit.edu>
2013-01-05 14:03:57 -0500

    License.

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject femur "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.3.0-beta3"]]
-            :main femur.server)
+  :description "A server for interacting with Dibble over common data formats"
+  :url "https://github.com/MichaelDrogalis/femur"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+                 [noir "1.3.0-beta3"]
+                 [dibble "0.2.0-beta1"]]
+  :main femur.server)
 
